060266d6c9237ae9ad2bf64a5bd87e430ad90feb
Jason Goodnight <Jasongoodnight3218@gmail.com>
2023-12-03 14:44:50 -0700

    Workshop Week 2

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -6,22 +6,22 @@ const partnerRouter = express.Router();
 partnerRouter.route('/')
   .get((req, res, next) => {
     Partner.find()
-      .then((partners) => {
+      .then(partners => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(partners);
       })
-      .catch((err) => next(err));
+      .catch(err => next(err));
   })
   .post((req, res, next) => {
     Partner.create(req.body)
-      .then((partner) => {
+      .then(partner => {
         console.log('Partner Created ', partner);
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(partner);
       })
-      .catch((err) => next(err));
+      .catch(err => next(err));
   })
   .put((req, res) => {
     res.statusCode = 403;
@@ -29,51 +29,47 @@ partnerRouter.route('/')
   })
   .delete((req, res, next) => {
     Partner.deleteMany()
-      .then((response) => {
+      .then(response => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(response);
       })
-      .catch((err) => next(err));
+      .catch(err => next(err));
   });
 
 partnerRouter.route('/:partnerId')
   .get((req, res, next) => {
     Partner.findById(req.params.partnerId)
-      .then((partner) => {
+      .then(partner => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(partner);
       })
-      .catch((err) => next(err));
+      .catch(err => next(err));
   })
   .post((req, res) => {
     res.statusCode = 403;
     res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
   })
   .put((req, res, next) => {
-    Partner.findByIdAndUpdate(
-      req.params.partnerId,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    )
-      .then((partner) => {
+    Partner.findByIdAndUpdate(req.params.partnerId, {
+      $set: req.body
+    }, { new: true })
+      .then(partner => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(partner);
       })
-      .catch((err) => next(err));
+      .catch(err => next(err));
   })
   .delete((req, res, next) => {
     Partner.findByIdAndDelete(req.params.partnerId)
-      .then((response) => {
+      .then(response => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(response);
       })
-      .catch((err) => next(err));
+      .catch(err => next(err));
   });
 
 module.exports = partnerRouter;
