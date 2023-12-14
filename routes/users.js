b9e5f8420ed8c83d55defbeb96a13fb39356e468
Jason Goodnight <Jasongoodnight3218@gmail.com>
2023-12-14 09:53:53 -0700

    Passport Facebook

--- routes/users.js
+++ routes/users.js
@@ -7,106 +7,74 @@ const cors = require('./cors');
 const router = express.Router();
 
 /* GET users listing. */
-router.get ( '/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin,function(req, res, next) {
-    User.find()
-    .then(user => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(user);
-    })
-    .catch(err => next(err));
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+  User.find()
+  .then(users => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json(users);
+  })
 });
 
 router.post('/signup', cors.corsWithOptions, (req, res) => {
-    User.register(
-        new User({username: req.body.username}),
-        req.body.password,
-        (err, user) => {
-            if (err) {
-                res.statusCode = 500;
-                res.setHeader('Content-Type', 'application/json');
-                res.json({err: err});
-            } else {
-                if (req.body.firstname) {
-                    user.firstname = req.body.firstname;
-                }
-                if (req.body.lastname) {
-                    user.lastname = req.body.lastname;
-                }
-                user.save(err => {
-                    if (err) {
-                        res.statusCode = 500;
-                        res.setHeader('Content-Type', 'application/json');
-                        res.json({err: err});
-                        return;
-                    }
-                    passport.authenticate('local')(req, res, () => {
-                        res.statusCode = 200;
-                        res.setHeader('Content-Type', 'application/json');
-                        res.json({success: true, status: 'Registration Successful!'});
-                    });
-                });
-            }
+  User.register(
+    new User({username: req.body.username}),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({err: err});
+      } else {
+        if (req.body.firstname) {
+          user.firstname = req.body.firstname;
         }
-    );
+        if (req.body.lastname) {
+          user.lastname = req.body.lastname;
+        }
+        user.save(err => { //save user info to server
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({err: err});
+            return;
+          }
+          passport.authenticate('local')(req, res, () => { //passport authenticate method uses local auth. strategy here
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({success: true, status: 'Registration successful!'});
+          });
+        })
+      }
+    }
+  );
 });
 
 router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
-    const token = authenticate.getToken({_id: req.user._id});
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({success: true, token: token, status: 'You are successfully logged in!'});
-});
-
-router.get('/login', cors.corsWithOptions, (req, res, next) => {
-    if(!req.session.user) {
-        const authHeader = req.headers.authorization;
-
-        if (!authHeader) {
-            const err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
-      
-        const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-        const username = auth[0];
-        const password = auth[1];
-      
-        User.findOne({username: username})
-        .then(user => {
-            if (!user) {
-                const err = new Error(`User ${username} does not exist!`);
-                err.status = 401;
-                return next(err);
-            } else if (user.password !== password) {
-                const err = new Error('Your password is incorrect!');
-                err.status = 401;
-                return next(err);
-            } else if (user.username === username && user.password === password) {
-                req.session.user = 'authenticated';
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('You are authenticated!')
-            }
-        })
-        .catch(err => next(err));
-    } else {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('You are already authenticated!');
-    }
+  const token = authenticate.getToken({_id: req.user._id});
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 router.get('/logout', cors.corsWithOptions, (req, res, next) => {
-    if (req.session) {
-        req.session.destroy();
-        res.clearCookie('session-id');
-        res.redirect('/');
-    } else {
-        const err = new Error('You are not logged in!');
-        err.status = 401;
-        return next(err);
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie('session-id');
+    res.redirect('/');
+  } else {
+    const err = new Error('You are not logged in.');
+    err.status = 401;
+    return next(err);
+  }
+});
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+    if (req.user) {
+        const token = authenticate.getToken({_id: req.user._id});
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: true, token: token, status: 'You are successfully logged in!'});
     }
 });
 
